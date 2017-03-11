bf36ac59222c00103772ff7d2a399bc148ffd4a8
smganzer <smganzert@ucdavis.edu>
2017-03-10 17:17:10 -0800

    login

--- test/script.js
+++ test/script.js
@@ -9,6 +9,22 @@ window.addEventListener("load", function(){
 
   });
 
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log(user.displayName);
+    } else {
+      // User is signed out.
+      console.log("User Logged Out")
+    }
+  });
 
 
 });
