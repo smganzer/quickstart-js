bca9efb56fdf6596fdc8afef8948eb1ca52f3734
smganzer <smganzert@ucdavis.edu>
2017-03-10 17:23:48 -0800

    sign out

--- test/script.js
+++ test/script.js
@@ -1,12 +1,17 @@
 window.addEventListener("load", function(){
 
   var signInButton = document.getElementById("sign-in-button");
-  signInButton.addEventListener("click", function(){
+  var signOutButton = document.getElementById("sign-out-button");
 
+  signInButton.addEventListener("click", function(){
       var provider = new firebase.auth.GoogleAuthProvider();
       firebase.auth().signInWithPopup(provider);
+  });
 
+  signOutButton.addEventListener("click", function(){
+      firebase.auth().signOut();
 
+      console.log("sign out")
   });
 
   firebase.auth().onAuthStateChanged(function(user) {
